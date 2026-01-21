4c3d69748eefe45afe29be09f5d977069b986b41
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2026-01-21 08:13:44 -0300

    Archivos y carpetas necesarias para comenzar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Pruebajoa from "./pages/pruebajoa";
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/pruebajoa" element={<Pruebajoa />} />
+        {/* Puedes agregar más rutas aquí */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ frontend/src/pages/estadosOdontograma.jsx
@@ -0,0 +1,8 @@
+export const ESTADOS = {
+  sano: { color: "white", simbolo: "" },
+  caries: { color: "red", simbolo: "" },
+  obturacion: { color: "blue", simbolo: "" },
+  ausente: { color: "white", simbolo: "X" },
+  endodoncia: { color: "blue", simbolo: "E" },
+  corona: { color: "blue", simbolo: "C" },
+};
